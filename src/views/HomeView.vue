bc7cf2677899c7c27c60101207d05fd440625ec6
William Shiao <willshiao@gmail.com>
2016-08-28 19:06:25 -0700

    feat: add homepage info

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,8 @@
 
 <div class="container">
   <h3>Home</h3>
+  <p>A static site providing client-side string manipulation, encoding, decoding, and encryption utilities.</p>
+  <p>Visit one of the pages in the navbar to get started.</p>
 </div>
 
 </template>
